be6163399920467cc8cf913e6437473c1999c16f
Chrisbuildit <115037055+Chrisbuildit@users.noreply.github.com>
2023-08-30 17:53:20 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AuthContextProvider from "./context/AuthContext";
+import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    // <React.StrictMode>
+    <Router>
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
+    </Router>
+    // </React.StrictMode>
 );
 
 
